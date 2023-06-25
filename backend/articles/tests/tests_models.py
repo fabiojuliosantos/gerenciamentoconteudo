7f4377345acd92276a031ee7ed7e7a888ec21c38
Fábio Júlio <fabiojulio.santos@protonmail.ch>
2023-06-25 02:06:43 -0300

    adicionando testes de tag e categoria

--- backend/articles/tests/tests_models.py
+++ backend/articles/tests/tests_models.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from articles.models import Article, Category, Tag
 
-# Create your tests here.
+
+class ArticleModelsTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.tag = Tag.objects.create(
+            name='python'
+        )
+
+        cls.category = Category.objects.create(
+            name='Programação'
+        )
+
+          
+    def test_tag_name_empty_field(self):
+        tag = Tag.objects.get(name='python')
+        self.assertEqual(tag.name, 'python')
+      
+    
+    def test_category_name_empty_field(self):
+        category = Category.objects.get(name='Programação')
+        self.assertEqual(category.name, 'Programação')
+    
+   
