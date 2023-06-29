4c15a1aa85c7fb4093440207338fcbc3c8b9f27e
Fábio Júlio <fabiojulio.santos@protonmail.ch>
2023-06-29 00:19:51 -0300

    alteração view

--- backend/articles/views.py
+++ backend/articles/views.py
@@ -7,6 +7,7 @@ from .serializers import ArticleSerializer
 # Create your views here.
 
 class ArticleView(CreateAPIView):
+    
     serializer_class = ArticleSerializer
 
     def post(self, request, *args, **kwargs):
