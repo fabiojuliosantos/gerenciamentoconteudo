213933e64dceca92e34edfcfcc9378d21f80a25e
Fábio Júlio <fabiojulio.santos@protonmail.ch>
2023-06-29 23:33:03 -0300

    Desenvolvimento da feature de atualização dos dados do usuário

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from .views import RegisterUserView, LoginUserView, RedefinePasswordView
+from .views import RegisterUserView, LoginUserView, RedefinePasswordView, UpdateUserView
 
 urlpatterns = [
     path('register', RegisterUserView.as_view()),
     path('login',LoginUserView.as_view()),
-    path('redefine_password', RedefinePasswordView.as_view())
+    path('redefine_password', RedefinePasswordView.as_view()),
+    path('update_user/<int:user_id>', UpdateUserView.as_view())
     ]
 
--- backend/users/views.py
+++ backend/users/views.py
@@ -3,6 +3,7 @@ from rest_framework import status
 from rest_framework.views import Response
 from rest_framework.generics import CreateAPIView
 from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 from .serializers import UserSerializer
 from django.core.mail import send_mail
 from django.core.exceptions import ObjectDoesNotExist
@@ -61,6 +62,7 @@ class LoginUserView(TokenObtainPairView):
         return Response(response_data, status=status.HTTP_200_OK)
 
 class RedefinePasswordView(APIView):
+    permission_classes = (IsAuthenticated, )
     def post(self, request):
         email = request.data.get('email')
 
@@ -70,13 +72,14 @@ class RedefinePasswordView(APIView):
             return Response({'error:' ' E-mail não encontrado no banco.'}, status=404)
         
         user_name = user.name
+        user_alias = user.username
         password = ''.join(random.choices(string.ascii_letters + string.digits, k=8))
 
         user.password = make_password(password)
         user.save()
 
         subject = 'Redefinição da senha do Sistema de Gerenciamento de Conteúdo'
-        message = f'Olá, {user_name}. Você solicitou a redefinição de sua senha, sua nova senha é: {password}, utilize-a para logar com seu username no sistema.'
+        message = f'Olá, {user_name}. você solicitou a redefinição de sua senha. \nOs dados do seu login são os seguintes:\n\nUsername: {user_alias} \nSenha: {password}'
         from_email = settings.DEFAULT_FROM_EMAIL
         recipient_list = [email]
         send_mail(subject, message, from_email, recipient_list)
@@ -94,3 +97,14 @@ class UpdateUserView(APIView):
         user_instance = self.get_object(user_id)
         if not user_instance:
             return Response({"res": f"Usuário com id={user_id} não existe"}, status=status.HTTP_400_BAD_REQUEST)
+        if user_instance != request.user:
+            return Response({"res": "Você não tem permissão para modificar esse usuário."}, status=status.HTTP_403_FORBIDDEN)
+
+        data = request.data
+
+        serializer = UserSerializer(instance=user_instance, data=data, partial=True)        
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
